a1719896e1decbda883a58f6e0ee50cb55c96da2
15Santiago <santiagorodriy@gmail.com>
2022-10-12 10:38:01 -0500

    Imagenes en el slider

--- main.js
+++ main.js
@@ -4,17 +4,24 @@ const robinButton = document.querySelector('.robin-btn');
 const batmanButton = document.querySelector('.batman-btn');
 const closeBtn = document.querySelector('.modal-button-close');
 
+const img1 = document.querySelector('#img-1');
+const img2 = document.querySelector('#img-2');
+const img3 = document.querySelector('#img-3');
+
 spiderButton.addEventListener('click', () => {
     modal.classList.remove('hidden');
     modal.classList.add('visible');
+    renderImgaes(spiderButton);
 });
 robinButton.addEventListener('click', () => {
     modal.classList.remove('hidden');
     modal.classList.add('visible');
+    renderImgaes(robinButton);
 });
 batmanButton.addEventListener('click', () => {
     modal.classList.remove('hidden');
     modal.classList.add('visible');
+    renderImgaes(batmanButton);
 });
 closeBtn.addEventListener('click', () => {
     modal.classList.add('hidden');
---
+++ uitlis.js
@@ -0,0 +1,19 @@
+function renderImgaes (superHero) {
+    switch (superHero) {
+        case spiderButton:
+            img1.setAttribute('src', 'https://static.platzi.com/media/public/uploads/spider-man_db8af43d-b692-4986-a385-e645d286e142.png');
+            img2.setAttribute('src', 'https://static.platzi.com/media/public/uploads/spider-man-2_b24c96b3-6d59-496f-815d-32b25eba0141.png');
+            img3.setAttribute('src', 'https://static.platzi.com/media/public/uploads/spider-man-3_19f5c510-8059-4477-abf8-42249a5445b7.png');
+        break;
+        case robinButton:
+            img1.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQofHpjb6jPfMOm13E4GRQwdPxkJuBR4407Sw&usqp=CAU');
+            img2.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtQ7StNNiKpdswIZSn0HW8WEUy-CgXpbgKxQ&usqp=CAU');
+            img3.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLNcKDHJSxjQVnUhSD1HbTILkQocTcAmwjUQ&usqp=CAU');
+        break;
+        case batmanButton:
+            img1.setAttribute('src', 'https://w7.pngwing.com/pngs/688/337/png-transparent-batman-lego-minifig-lego-batman-2-dc-super-heroes-joker-lego-batman-the-videogame-lego-dimensions-the-lego-movie-heroes-superhero-fictional-character.png');
+            img2.setAttribute('src', 'https://www.cinemascomics.com/wp-content/uploads/2016/09/lego-batman.jpg');
+            img3.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTczAb6EhSyNzWdHmtffnNfOFxhcbmE1tswIg&usqp=CAU');
+        break;
+    }
+};
